92711bf22b3875bad239d169dd2e29a9f31e05f4
StyxS <mail@styxs.cn>
2018-11-22 17:21:45 +0800

    add semaphore to control schedule-process

---
+++ ProcessSchedule/Schedule.cpp
@@ -0,0 +1,10 @@
+/*
+ * Schedule.cpp
+ *
+ *  Created on: 2018年11月22日
+ *      Author: styxs
+ */
+#include <semaphore.h>
+#include "Schedule.h"
+
+sem_t needSchedule, scheduleUp;
--- ProcessSchedule/Schedule.h
+++ ProcessSchedule/Schedule.h
@@ -4,6 +4,7 @@
  *  Created on: 2018年11月22日
  *      Author: styxs
  */
+#include <semaphore.h>
 
 #ifndef SCHEDULE_H_
 #define SCHEDULE_H_
@@ -22,6 +23,10 @@ typedef struct{
 	unsigned int arraySize;
 	int methodIndex; // indicate to choose which method to schedule
 }scheduleParamter;
+
+extern sem_t needSchedule, scheduleUp; // all process wait scheduleUp to check their PCB to know who can run, \
+	and when the one stop to run it set the needSchedule to wake up schedule Function \
+	(So it should be define before calling schedule and processFunction)
 
 /* choose a process to run by changing it's PCB*/
 void schedule(void *pParameter);
